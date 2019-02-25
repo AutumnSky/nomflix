cdfa111bf5195b159245e5724e07ebf370c33316
MJ K <capu1004@gmail.com>
2019-02-25 14:02:53 +0900

    Updates

---
+++ precache-manifest.8f8f777b419ca7bf893697849be7b8c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1b457ea1da362405488",
+    "url": "/nomflix/static/js/main.c1b457ea.chunk.js"
+  },
+  {
+    "revision": "15cd60d1048286358a73",
+    "url": "/nomflix/static/js/runtime~main.15cd60d1.js"
+  },
+  {
+    "revision": "fc0d33593a8bf8782f85",
+    "url": "/nomflix/static/js/2.fc0d3359.chunk.js"
+  },
+  {
+    "revision": "f7f2904f1f3f39d9b1ff2400c30bcecc",
+    "url": "/nomflix/static/media/imdb.f7f2904f.png"
+  },
+  {
+    "revision": "0cfd8ac4d494f9404fb964898f36154f",
+    "url": "/nomflix/index.html"
+  }
+];
